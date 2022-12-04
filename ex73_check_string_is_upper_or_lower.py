00f94b32e548fb2942692dfe2fe030061c7f99a9
florin7755 <116590232+florin7755@users.noreply.github.com>
2022-12-04 21:29:54 +0200

    Add files via upload

---
+++ ex73_check_string_is_upper_or_lower.py
@@ -0,0 +1,14 @@
+string=("HELLO")
+print("string=",string)
+x=string.isupper()
+print('x=',x)
+if x==True:
+    print("It's uppercase")
+    
+    
+string=("hello")
+print("string=",string)
+x=string.isupper()
+print('x=',x)
+if x!=True:
+    print("It's lowercase")
